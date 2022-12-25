43af94f8b3400c59e1bce4f649278199189d9504
felix <f.o.d.c@web.de>
2022-12-25 15:04:49 +0100

    Add Sample Test

---
+++ src/tests/students/TestClass.java
@@ -0,0 +1,17 @@
+package tests.students;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import ui.GameplayState;
+
+public class TestClass {
+
+    @Test
+    public final void firstTest() {
+        String testString = "TestSetup";
+        assertEquals("TestSetp",testString);
+    }
+}
+
