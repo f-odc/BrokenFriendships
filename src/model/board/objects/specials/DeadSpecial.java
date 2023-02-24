91b072821f54a74c7f8236aa8dab97ee81dfd486
f-odc <f.o.d.c@web.de>
2023-02-24 16:56:13 +0100

    Allow new turn if 6 is thrown

--- src/model/board/objects/specials/DeadSpecial.java
+++ src/model/board/objects/specials/DeadSpecial.java
@@ -4,6 +4,7 @@ import eea.engine.entity.Entity;
 import model.board.fields.IField;
 import model.board.objects.IGameObject;
 import model.game.logic.GameLogic;
+import model.global;
 
 public class DeadSpecial implements IGameObject {
 
@@ -16,7 +17,10 @@ public class DeadSpecial implements IGameObject {
     @Override
     public void activate(IGameObject sourceGameObject) {
         sourceGameObject.reset();
-        GameLogic.nextPlayer();
+        // only next player if
+        if (global.BOARD.getDice().getValue() != 6){
+            GameLogic.nextPlayer();
+        }
     }
 
     @Override
