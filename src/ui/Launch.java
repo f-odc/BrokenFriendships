40f0ee4513b405c309d1e7c68674fa11ea92b62b
Marvin <marvin.christian@stud.tu-darmstadt.de>
2023-02-25 14:22:35 +0100

    changed figure design

--- src/ui/Launch.java
+++ src/ui/Launch.java
@@ -40,7 +40,7 @@ public class Launch extends StateBasedGame {
         AppGameContainer app = new AppGameContainer(new Launch());
  
         // Lege die Einstellungen des Fensters fest und starte das Fenster
-        app.setDisplayMode(global.X_DIMENSIONS, global.Y_DIMENSIONS, true);
+        app.setDisplayMode(global.X_DIMENSIONS, global.Y_DIMENSIONS, false);
         app.start();
     }
 
